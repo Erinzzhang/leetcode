5564d10c22abee83a358baf42d297baf03a2473f
ErinZhang <b10231034@gmail.com>
2018-08-08 00:47:38 +0800

    Update 1. Remove Duplicates from Sorted Array.cpp

--- Erin/Easy Collection Top Interview Questions/1. Remove Duplicates from Sorted Array.cpp
+++ Erin/Easy Collection Top Interview Questions/1. Remove Duplicates from Sorted Array.cpp
@@ -15,15 +15,15 @@ copyright(c) 2018 Ya-Han Chang
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int temp;
+
         for (int i = 0; i < nums.size(); i++)
         {
-            temp = nums[i];
-            if(i + 1 < nums.size() && temp == nums[i+1])
+            if(i + 1 < nums.size() && nums[i] == nums[i+1])
             {
-                nums.erase(nums.begin()+i);;
+                nums.erase(nums.begin()+i);
                 i--;
             }
+            
         }
         return nums.size();
     }
